a6e7e410787a71477112dcaa68006c25567d3685
akashsingh7202@gmail.com <akashsingh7202@gmail.com>
2024-11-10 16:57:42 +0530

    Updated

--- front_end/src/pages/tasks.jsx
+++ front_end/src/pages/tasks.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import TaskList from "../components/Task/task_list";
 import { AuthContext } from "../context/authContext";
 import { useNavigate } from "react-router-dom";
-import "./task.css";
 
 const Tasks = () => {
   const { logout } = useContext(AuthContext);
@@ -10,16 +9,14 @@ const Tasks = () => {
 
   const handleLogout = () => {
     logout();
-    alert("You have been logged out successfully.");
+    alert("Logged out successfully!");
     navigate("/login");
   };
 
   return (
-    <div className="tasks-container">
-      <h2 className="tasks-heading">Your Task Dashboard</h2>
-      <button className="logout-button" onClick={handleLogout}>
-        Logout
-      </button>
+    <div>
+      <h2>Your Tasks</h2>
+      <button onClick={handleLogout}>Logout</button>
       <TaskList />
     </div>
   );
